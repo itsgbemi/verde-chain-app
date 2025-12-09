fade77769d94a6eaad1a63ac7e27fedba022df2a
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-12-09 14:35:28 -0800

    Add App.vue with layout and routing components

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="app">
+<AppHeader v-if="$route.path!=='/login'"/>
+<div class="main-content">
+<RouterView/>
+</div>
+<AppFooter v-if="$route.path!=='/login'"/>
+<NotificationCenter/>
+<ConnectWalletModal v-if="showWalletModal"/>
+</div>
+</template>
+<script setup>
+import{ref}from'vue'
+import{useRoute}from'vue-router'
+import AppHeader from'@/components/layout/AppHeader.vue'
+import AppFooter from'@/components/layout/AppFooter.vue'
+import NotificationCenter from'@/components/common/NotificationCenter.vue'
+import ConnectWalletModal from'@/components/wallet/ConnectWalletModal.vue'
+const route=useRoute()
+const showWalletModal=ref(false)
+</script>
+<style scoped>
+.main-content{
+min-height:calc(100vh - 120px);
+}
+</style>
